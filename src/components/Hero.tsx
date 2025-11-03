fb6fcad9dbd4929332b4c96161c1e771af9a9fad
Rithesh Roshan R <ritheshroshanr@Ritheshs-MacBook-Pro.local>
2025-11-03 13:17:01 +0530

    changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -268,7 +268,7 @@ const Hero = () => {
               initial={{ opacity: 0, y: 25 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 1, delay: 0.6 }}
-              className="mt-10 px-48"
+              className="mt-10 flex justify-center"
             >
               <Button
                 size="lg"
